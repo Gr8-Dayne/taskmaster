f4eb2cc59f04a81a8d9630d5a0dded4c42308672
Dayne Daylong <dalexad@gmail.com>
2020-02-11 13:13:37 -0800

    competed feature tasks

--- app/src/main/java/com/daylong/taskmaster/MainActivity.java
+++ app/src/main/java/com/daylong/taskmaster/MainActivity.java
@@ -2,7 +2,10 @@ package com.daylong.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonTaskAdd = findViewById(R.id.addTaskButton);
+        buttonTaskAdd.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                Intent goToAddTask = new Intent (MainActivity.this, AddTask.class);
+                MainActivity.this.startActivity(goToAddTask);
+            }
+        });
+
+        Button buttonViewAllTasks = findViewById(R.id.viewAllTasks);
+        buttonViewAllTasks.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                Intent goToAddTask = new Intent (MainActivity.this, AllTasks.class);
+                MainActivity.this.startActivity(goToAddTask);
+            }
+        });
     }
 }
